7ae46e537dccb98f4ae8b98d735c41761d24bcbe
WilliamMOgle <oglewilli@gmail.com>
2021-04-30 19:39:03 -0400

    enable statements

--- XRTIC_Full Integration/main.c
+++ XRTIC_Full Integration/main.c
@@ -1,40 +1,5 @@
-//*****************************************************************************
-//
-// main.c
-//
-// Copyright (c) 2015 Texas Instruments Incorporated.  All rights reserved.
-// TI Information - Selective Disclosure
-//
-//*****************************************************************************
-
 #include "main.h"
 
-
-
-
-bool pollDistanceSensor(void){
-  if(OPT3101_CheckDistanceSensor()){
-    TxChannel = OPT3101_GetMeasurement(Distances,Amplitudes);
-    return true;
-  }
-  return false;
-}
-
-// calibrated for 500mm track
-// right is raw sensor data from right sensor
-// return calibrated distance from center of Robot to right wall
-int32_t Right(int32_t right){
-  return  (right*(59*right + 7305) + 2348974)/32768;
-}
-// left is raw sensor data from left sensor
-// return calibrated distance from center of Robot to left wall
-int32_t Left(int32_t left){
-  return (1247*left)/2048 + 22;
-}
-
-//END SENSOR STUFF
-
-void printString(char output[]);
 
 #ifdef MSP432P401R_LAUNCHPAD_ENABLED
 #include "lp_buttons.h"
@@ -42,10 +7,6 @@ void printString(char output[]);
 
 #define DOGS102x6_DRAW_NORMAL 0x00
 #define DOGS102x6_DRAW_INVERT 0x01
-
-//ROVER AND MQTT STUFF
-
-
 
 /*
  * GLOBAL VARIABLES -- Start
@@ -55,143 +16,99 @@ volatile unsigned int S1buttonDebounce = 0;
 volatile unsigned int S2buttonDebounce = 0;
 volatile int publishID = 0;
 
-//
-// Number of bytes received from the host
-//
-volatile uint16_t g_ui16BytesReceived = 0x00;
-/*
- * GLOBAL VARIABLES -- End
- */
-
-//END ROVER AND MQTT STUFF
-
 
 int main(int argc, char** argv)
 {
 
     // Initialize MCU
     MCU_init();
+    MAP_WDT_A_holdTimer();
+    initialize_LaunchpadLEDs();
     initUART();
 
-    bool tagReseted = true;
-
-    tNfcState eTempNFCState;
-    tNfcState eCurrentNFCState;
-
-    // CE Variables
-    t_sNfcCEMode sCEMode;
-
-    t_sNfcP2PMode sP2PMode;
-    t_sNfcP2PCommBitrate sP2PBitrate;
-
-    // Reader/Writer RX Status
-    t_sNfcRWMode sRWMode;
-    t_sNfcRWCommBitrate sRWBitrate;
-
-    NFC_completeInit();
-    initialize_LaunchpadLEDs();
-    button_two_interrupt_init();
-
-    //Init Timer
-    initSWTimer1();
-    //updateSW1WaitCycles(50000); //0.1ms per cycle
+    //Helpful Timer - NOT REQUIRED TO FUNCTION
     Init_Timer32_0(TIMER32_INIT_COUNT, CONTINUOUS);
+    initSWTimer1();
+    initSWTimer2();
+    updateSW1WaitCycles(30000);
+    //updateSW2WaitCycles(1000);
 
-    //7 segment initialization of outputs
-    initializeOutputs();
+#if ROVER_ENABLE
+    roverInit();
+#endif
+
+#if BUMP_ENABLE
     initBumpSensors();
+#endif
 
-    //START SETUP FOR ROVER AND MQTT
-    MAP_WDT_A_holdTimer();
+#if SEG7_ENABLE
+    initializeOutputs();//7seg
+    //turn 7 segment on to 0
+    segmentWrite('0');
+#endif
 
-    //rslk rover init
-    initRSLKRover(SYS_CLK);
-    initRSLKTimer32(TIMER32_1_BASE);
-    enableWheel(&right_wheel_data);
-    setWheelDirForward(&right_wheel_data);
-    enableWheel(&left_wheel_data);
-    setWheelDirForward(&left_wheel_data);
-    enableWheelCompensator(&left_wheel_data);
-    enableWheelCompensator(&right_wheel_data);
-    speed_state = SPEED_HOLD;
-    speed = 100;
-    stopRover();
+#if NFC_ENABLE
+    NFC_completeInit();
+    button_two_interrupt_init();
+    g_ui16BytesReceived = 0x00;
+    uint8_t count = 2;
+    bool tag_present = false;
+#endif
 
-    transmitString("Hey");
-
-    _i32 retVal = -1;
-    retVal = initializeAppVariables();
-    ASSERT_ON_ERROR(retVal);
-
-    displayBanner();
-
+#if MQTT_ENABLE
     int rc = 0;
     unsigned char buf[100];
     unsigned char readbuf[100];
+    _i32 retVal = -1;
 
-
-    //SENSOR SET UP
-    uint32_t channel = 1;
-    SysTick->LOAD = 0x00FFFFFF;           // maximum reload value
-    SysTick->CTRL = 0x00000005;           // enable SysTick with no interrupts
-    I2CB1_Init(30); // baud rate = 12MHz/30=400kHz
-    OPT3101_Init();
-    OPT3101_Setup();
-    OPT3101_CalibrateInternalCrosstalk();
-    OPT3101_ArmInterrupts(&TxChannel, Distances, Amplitudes);
-    StartTime = SysTick->VAL;
-    OPT3101_StartMeasurement();
-    //END IR INIT
-#if MQTT_ENABLE
+    retVal = initializeAppVariables();
+    ASSERT_ON_ERROR(retVal);
     setUpMQTT(retVal, buf, readbuf, rc);
-#endif
 
-    //message fore 7 segment
+    MQTTMessage msgRFID;
+    MQTTMessageInit(&msgRFID);
+
     MQTTMessage msg7Seg;
     MQTTMessageInit(&msg7Seg);
+#endif
 
-    //turn 7 segment on to 0
-    segmentWrite('0');
+#if IR_ENABLE
+    uint32_t channel = 1;
+    irInit();
+#endif
+
+#if GRIPPER_ENABLE
+    Servo180 servoSettings;
+    servo180InitArgs(&servoSettings,SYS_CLK,140,80,GPIO_PORT_P8,GPIO_PIN2);
+    closeServo(&servoSettings);
+#endif
+
+#if (ROBONAV_ENABLE & MQTT_ENABLE)
+    bool robonav_on = false;
+#endif
+
+    displayBanner();
+
+    //message fore 7 segment
     //msg7Seg.payload = "{\"sA\":1,\"sB\":1,\"sC\":1,\"sD\":1,\"sE\":1,\"sF\":1,\"sG\":0,\"sDP\":0}";
     //msg7Seg.payloadlen = strlen(msg7Seg.payload);
     //rc = MQTTPublish(&hMQTTClient, "XRTIC20/Feedback/SevenSegmentDisplay", &msg7Seg);
 
-
-    //RFID POSITIVE
-
-     MQTTMessage msgRFID;
-     MQTTMessageInit(&msgRFID);
-
-
-     //INIT GRIPPER
-
-     Servo180 servoSettings;
-     //double degree = 145;
-     //bool countDir = false;
-
-     Init_Timer32_0(TIMER32_INIT_COUNT, CONTINUOUS);
-     initSWTimer1();
-     initSWTimer2();
-     updateSW1WaitCycles(30000);
-
-     servo180InitArgs(&servoSettings,SYS_CLK,140,80,GPIO_PORT_P8,GPIO_PIN2);
-     closeServo(&servoSettings);
-
-     //END GRIPPER INIT
-
+    //Clock Check
     transmitString("MCLK: ");
     transmitInt(CS_getMCLK());
-    SW_Timer_1.elapsedCycles = 0;
+    //SW_Timer_1.elapsedCycles = 0;
 
-    uint8_t count = 2;
-    bool tag_present = false;
     while(1)
     {
+#if ROVER_ENABLE
         //Rover updates
         checkEndCond();
         updateRoverState();
         compensator();
+#endif
 
+#if TELEM_ENABLE
         //NON-BLOCKING TELEMETRY SEND CHECK
         if(SW1TimerRollover())
         {
@@ -230,7 +147,9 @@ int main(int argc, char** argv)
             //END SERVO DEMO
         }
         //END NON-BLOCKING TELEMETRY SEND CHECK
+#endif
 
+#if NFC_ENABLE
         //NFC ENABLE STATE MACHINE
         if(recMQTTData.newData)
         {
@@ -248,176 +167,44 @@ int main(int argc, char** argv)
         }
         //END NFC ENABLE STATE MACHINE
 
-
         if(recMQTTData.nfcEnabled)
         {
-            transmitString("In NFC");transmitNewLine();
-            eTempNFCState = NFC_run();
-
-            if(eTempNFCState == NFC_DATA_EXCHANGE_PROTOCOL)
-            {
-
-                if(count != 2)
-                {
-                    count = 2;
-                }
-
-                if(NFC_RW_getModeStatus(&sRWMode,&sRWBitrate))
-                {
-    #if NFC_READER_WRITER_ENABLED
-                    NFC_RW_LED_POUT |= NFC_RW_LED_BIT;
-
-                    if( sRWMode.bits.bNfcA == 1)
-                    {
-                        if(NFC_A_getSAK() == 0x00)
-                        {
-                            // T2T Tag State Machine
-                            if(tag_present == false)
-                            {
-                                turnOn_LaunchpadLED2_green();
-                                transmitString("Type 2 Tag Detect\n\r");
-                                tag_present = true;
-                                //turn 7 segment on to 0
-                                segmentWrite('b');
-                               // msg7Seg.payload = "{\"sA\":0,\"sB\":0,\"sC\":1,\"sD\":1,\"sE\":1,\"sF\":1,\"sG\":1,\"sDP\":0}";
-                                //msg7Seg.payloadlen = strlen(msg7Seg.payload);
-                                //rc = MQTTPublish(&hMQTTClient, "XRTIC20/Feedback/SevenSegmentDisplay", &msg7Seg);
-
-
-                                //msgRFID.payload = "{\"type\":2,\"effect\":\"None\"}";
-                                //msgRFID.payloadlen = strlen(msgRFID.payload);
-                                //rc = MQTTPublish(&hMQTTClient, "XRTIC20/Feedback/RFID", &msgRFID);
-                            }
-                            T2T_stateMachine();
-                        }
-                        else if(NFC_A_getSAK() & 0x20)
-                        {
-                            // T4T Tag State Machine
-                            T4T_stateMachine();
-                        }
-                    }
-                    else if(sRWMode.bits.bNfcB == 1)
-                    {
-                        if(NFC_B_isISOCompliant())
-                        {
-                            // T4T Tag State Machine
-                            T4T_stateMachine();
-                        }
-                    }
-                    else if(sRWMode.bits.bNfcF == 1)
-                    {
-                        // T3T Tag State Machine
-                        T3T_stateMachine();
-                    }
-                    else if(sRWMode.bits.bISO15693 == 1)
-                    {
-                        // T5T Tag State Machine
-                        if(tag_present == false)
-                        {
-                            turnOn_LaunchpadLED2_blue();
-                            transmitString("Type 5 Tag Detect\n\r");
-                            //turn 7 segment on to 0
-                            segmentWrite('a');
-                            tag_present = true;
-                            //msg7Seg.payload = "{\"sA\":1,\"sB\":1,\"sC\":1,\"sD\":0,\"sE\":1,\"sF\":1,\"sG\":1,\"sDP\":0}";
-                            //msg7Seg.payloadlen = strlen(msg7Seg.payload);
-                            //rc = MQTTPublish(&hMQTTClient, "XRTIC20/Feedback/SevenSegmentDisplay", &msg7Seg);
-
-                            ///msgRFID.payload = "{\"type\":5,\"effect\":\"None\"}";
-                            //msgRFID.payloadlen = strlen(msgRFID.payload);;
-                            //rc = MQTTPublish(&hMQTTClient, "XRTIC20/Feedback/RFID", &msgRFID);
-                        }
-                    }
-    #endif
-                }
-                else if(NFC_P2P_getModeStatus(&sP2PMode,&sP2PBitrate))
-                {
-
-                }
-                else if(NFC_CE_getModeStatus(&sCEMode))
-                {
-
-                }
-
-                // Update only RSSI
-               //updateLcdfcStatus(true);
-            }
-            else
-            {
-                // Clear LEDs (RX & TX)
-                if(count != 0)
-                    count--;
-
-                if(count == 0)
-                {
-                   turnOff_LaunchpadLED1();
-                   turnOff_LaunchpadLED2_red();//LaunchpadLED2_green
-                   turnOff_LaunchpadLED2_green();
-                   turnOff_LaunchpadLED2_blue();
-
-                   NFC_RW_LED_POUT &= ~NFC_RW_LED_BIT;
-                   NFC_P2P_LED_POUT &= ~NFC_P2P_LED_BIT;
-                   NFC_CE_LED_POUT &= ~NFC_CE_LED_BIT;
-                   tag_present = false;
-                }
-
-            }
-
-            // Update Current State if it has changed.
-            if(eCurrentNFCState != eTempNFCState)
-            {
-                __no_operation();
-
-                if(eCurrentNFCState != NFC_TARGET_WAIT_FOR_ACTIVATION
-                    && eCurrentNFCState != NFC_STATE_IDLE
-                    && (eTempNFCState == NFC_PROTOCOL_ACTIVATION
-                        || eTempNFCState == NFC_DISABLED))
-                {
-                    eCurrentNFCState = eTempNFCState;
-
-    #if NFC_READER_WRITER_ENABLED
-                    // Initialize the RW T2T, T3T, T4T and T5 state machines
-                    T2T_init(g_ui8TxBuffer,256);
-                    T5T_init(g_ui8TxBuffer,256);
-    #endif
-                    //turn 7 segment on to 0
-                    segmentWrite('0');
-                   // msg7Seg.payload = "{\"sA\":1,\"sB\":1,\"sC\":1,\"sD\":1,\"sE\":1,\"sF\":1,\"sG\":0,\"sDP\":0}";
-                    //msg7Seg.payloadlen = strlen(msg7Seg.payload);
-                    //rc = MQTTPublish(&hMQTTClient, "XRTIC20/Feedback/SevenSegmentDisplay", &msg7Seg);
-
-                    buttonDebounce = 1;
-                }
-                else
-                {
-                    eCurrentNFCState = eTempNFCState;
-                }
-
-            }
-
-            // Check if any packets have been received from the NFC host.
-            if(g_ui16BytesReceived > 0)
-            {
-                Serial_processCommand();
-            }
-
-            //transmitString("END NFC \n\r");
-            //transmitInt(SW_Timer_1.elapsedCycles);
-            //transmitString("\n\r");
-            //SW_Timer_1.elapsedCycles = 0;
-               Serial_processCommand();
-
+            nfc_tag_detect(&tag_present, &count);
         }
+#endif
 
-        //transmitString("START MQTT \n\r");
-
-
+#if MQTT_ENABLE
         rc = MQTTYield(&hMQTTClient, 10);
         if (rc != 0) {
             transmitString(" MQTT failed to yield \n\r");
             LOOP_FOREVER();
         }
 
+        //MessageData data1;
+        //messageArrived(&data1);
+
+        if (publishID) {
+            int rc = 0;
+            MQTTMessage msg;
+            msg.dup = 0;
+            msg.id = 0;
+            msg.payload = uniqueID;
+            msg.payloadlen = 8;
+            msg.qos = QOS0;
+            msg.retained = 0;
+            rc = MQTTPublish(&hMQTTClient, PUBLISH_TOPIC, &msg);
+
+            if (rc != 0) {
+                transmitString(" Failed to publish unique ID to MQTT broker \n\r");
+                LOOP_FOREVER();
+            }
+            transmitString(" Published unique ID successfully \n\r");
+
+            publishID = 0;
+        }
+#endif
+
+#if GRIPPER_ENABLE
         //GRIPPER STATE MACHINE
         if(recMQTTData.newData)
         {
@@ -436,10 +223,10 @@ int main(int argc, char** argv)
 
             }
         }
+#endif
 
+#if (ROVER_ENABLE & MQTT_ENABLE)
         //ROVER STATE MACHINE - BEGIN
-        //Speed setting
-        //transmitString("Speed state: ");
         switch(speed_state)
         {
             case SPEED_INCREASE: speed += 10; /*transmitString("SPEED_INCREASE\n\r");*/break;
@@ -448,9 +235,6 @@ int main(int argc, char** argv)
             default: break;
         }
         //transmitInt(speed);transmitNewLine();
-
-        //motor contorl
-        //uint16_t speed = ROVER_SPEED;
         if(recMQTTData.newData)
         {
             if(recMQTTData.pressed)
@@ -539,37 +323,13 @@ int main(int argc, char** argv)
 
                 //recMQTTData.newData = false;
             }
-            segmentWrite('F');
+            //segmentWrite('F');
             recMQTTData.newData = false;
         }
 
-        //END
+#endif
 
-
-        //MessageData data1;
-        //messageArrived(&data1);
-
-        if (publishID) {
-            int rc = 0;
-            MQTTMessage msg;
-            msg.dup = 0;
-            msg.id = 0;
-            msg.payload = uniqueID;
-            msg.payloadlen = 8;
-            msg.qos = QOS0;
-            msg.retained = 0;
-            rc = MQTTPublish(&hMQTTClient, PUBLISH_TOPIC, &msg);
-
-            if (rc != 0) {
-                transmitString(" Failed to publish unique ID to MQTT broker \n\r");
-                LOOP_FOREVER();
-            }
-            transmitString(" Published unique ID successfully \n\r");
-
-            publishID = 0;
-        }
-
-
+#if BUMP_ENABLE
         //BUMP SENSOR CHECKS
         if(bumpSensorPressed(BUMP0))
             transmitString("Bump 0 Pressed!\n\r");
@@ -583,8 +343,33 @@ int main(int argc, char** argv)
             transmitString("Bump 4 Pressed!\n\r");
         if(bumpSensorPressed(BUMP5))
             transmitString("Bump 5 Pressed!\n\r");
+#endif
 
+#if IR_ENABLE
+        OPT3101_StartMeasurementChannel(channel);
+        if(channel < 2)
+        {
+            channel++;
+        }
+        else
+        {
+            channel = 0;
+        }
+        OPT3101_GetMeasurement(Distances,Amplitudes);
 
+        transmitString("Left: ");
+        transmitInt(Distances[0]);
+        transmitString("mm | ");
+        transmitString("Center: ");
+        transmitInt(Distances[1]);
+        transmitString("mm | ");
+        transmitString("Right: ");
+        transmitInt(Distances[2]);
+        transmitString("mm\n\r");
+
+#endif
+
+#if (IR_ENABLE & NFC_ENABLE)
         //MINIMAL SENSOR STUFF
         if(!tag_present)
         {
@@ -609,29 +394,33 @@ int main(int argc, char** argv)
             transmitInt(Distances[2]);
             transmitString("mm\n\r");
         }
-
-
-        /*sensorIRData.leftDistance = Distances[0];               //generate MQTT message
-        sensorIRData.rightDistance = Distances[1];
-        sensorIRData.centerDistance = Distances[2];
-
-        char * payload;
-        payload = (char*) malloc(50 * sizeof(char));
-        convertSensorIRToJSONString(sensorIRData, &payload, 50);
-        int x = 1;*/
-
         //END MINIMAL SENSOR STUFF
+#endif
 
-        if(ROBONAV_ENABLE)
+#if (IR_ENABLE & BUMP_ENABLE & ROBONAV_ENABLE & MQTT_ENABLE & ROVER_ENABLE)
+        //enables with "robonav"
+
+        if(recMQTTData.newData)
         {
-            roboNav();
+            if(recMQTTData.pressed && !strcmp(recMQTTData.key, "robonav"))
+            {
+                robonav_on = true;
+                recMQTTData.newData = false;
+            }
+            else if(!recMQTTData.pressed && !strcmp(recMQTTData.key, "robonav"))
+            {
+                robonav_on  = false;
+                recMQTTData.newData = false;
+                hardStop();
+                //eTempNFCState == NFC_PROTOCOL_ACTIVATION;
+            }
         }
 
+        if(robonav_on)
+            roboNav();
+#endif
+
     }
-
-
-
-
 }
 
 
@@ -1271,15 +1060,7 @@ void EUSCIA0_IRQHandler(void)
     }
 
 }*/
-void printString(char output[])
-{
-    int size = sizeof output / sizeof output[0];
-    int i;
-    for(i = 0; i < size; i++)
-    {
-        UART_transmitData(EUSCI_A0_BASE, output[i]);
-    }
-}
+
 //*****************************************************************************
 //
 //! NFC_configuration - Handles the initial NFC configuration.
